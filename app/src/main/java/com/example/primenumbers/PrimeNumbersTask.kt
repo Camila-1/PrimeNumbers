e82bd995a767fa4c4ba48065fdf6a7a2b45b51c2
Camila <camilabalova@gmail.com>
2019-11-24 12:53:12 +0200

    apk

--- app/src/main/java/com/example/primenumbers/PrimeNumbersTask.kt
+++ app/src/main/java/com/example/primenumbers/PrimeNumbersTask.kt
@@ -4,7 +4,7 @@ import android.os.AsyncTask
 
 class PrimeNumbersTask (private val adapter: Adapter, private val showAlertDialog: (Int) -> Unit) : AsyncTask<Long, Long, List<Long>>(){
 
-    private val primeNumbers = PrimeNumbers(1000000, adapter.numbers.toList().toMutableList()) {
+    private val primeNumbers = PrimeNumbers(100, adapter.numbers.toList().toMutableList()) {
         Thread.sleep(100)
         publishProgress(it)
     }
